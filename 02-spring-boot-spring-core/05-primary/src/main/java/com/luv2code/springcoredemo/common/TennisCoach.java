604c2e4fd4ccbce27734d8ffe0d71880ffbaa14a
Alaa eldin apuelsoad <149531307+AlaaApuelsoad@users.noreply.github.com>
2024-05-19 23:46:21 +0300

    Add files via upload

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Tennis Coach: practice your backhand volley ";
+    }
+}
