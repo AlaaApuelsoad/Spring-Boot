a32f369b6c70738ef491120c8835c17ae5a2bd4c
Alaa eldin apuelsoad <149531307+AlaaApuelsoad@users.noreply.github.com>
2024-05-20 15:47:08 +0300

    Add files via upload

---
+++ 10-spring-boot-aop/04-spring-boot-aop-demo-match-for-param-pattern/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,25 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+
+    // this is where we add all of our related advices for logging
+
+    //match method that have Account parameter and any number of parameters
+    // @Before("execution(* add*(com.luv2code.aopdemo.Account,..))")
+
+    //match on any parameters
+    //@Before("execution(* add*(..))")
+
+    //match package with any class,methods,any number of parameters
+    @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    public void beforeAddAccountAdvice(){
+
+        System.out.println("\n ======>>> Executing @Before advice on addAccount() ");
+    }
+}
